c8948bdb1570877599b8198d8d8a309b349a9b2e
Simon Guillorit <simon.guillorit@gmail.com>
2017-11-28 16:38:07 +0100

    Install Mention.js, no implementation in UI yet

--- app/assets/javascripts/writing.js
+++ app/assets/javascripts/writing.js
@@ -23,3 +23,50 @@ var editor = new MediumEditor('.editable', {
       hideOnClick: true
   }
 });
+
+// $(document).ready(function(){
+
+//   $(".content-writing-text").mention({
+//     users: [{
+//       name: 'Lindsay Made',
+//       username: 'LindsayM',
+//       image: 'http://placekitten.com/25/25'
+//     }, {
+//       name: 'Rob Dyrdek',
+//       username: 'robdyrdek',
+//       image: 'http://placekitten.com/25/24'
+//     }, {
+//       name: 'Rick Bahner',
+//       username: 'RickyBahner',
+//       image: 'http://placekitten.com/25/23'
+//     }, {
+//       name: 'Jacob Kelley',
+//       username: 'jakiestfu',
+//       image: 'http://placekitten.com/25/22'
+//     }, {
+//       name: 'John Doe',
+//       username: 'HackMurphy',
+//       image: 'http://placekitten.com/25/21'
+//     }, {
+//       name: 'Charlie Edmiston',
+//       username: 'charlie',
+//       image: 'http://placekitten.com/25/20'
+//     }, {
+//       name: 'Andrea Montoya',
+//       username: 'andream',
+//       image: 'http://placekitten.com/24/20'
+//     }, {
+//       name: 'Jenna Talbert',
+//       username: 'calisunshine',
+//       image: 'http://placekitten.com/23/20'
+//     }, {
+//       name: 'Street League',
+//       username: 'streetleague',
+//       image: 'http://placekitten.com/22/20'
+//     }, {
+//       name: 'Loud Mouth Burrito',
+//       username: 'Loudmouthfoods',
+//       image: 'http://placekitten.com/21/20'
+//     }]
+//   });
+// });
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap-typehead.js )
+Rails.application.config.assets.precompile += %w( mention.js )
