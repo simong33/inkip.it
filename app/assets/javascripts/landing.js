207014cdb154a1d03d0e52bd31c2b1db6f4fd18e
Simon Guillorit <simon.guillorit@gmail.com>
2017-12-20 15:40:02 +0100

    Fix JS flow

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,6 +1,8 @@
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.value-proposition-block', {
-  duration: 1000,
-  origin: 'top',
-  reset: false,
+$(document).ready(function(){
+  window.sr = ScrollReveal({ reset: true });
+  sr.reveal('.value-proposition-block', {
+    duration: 1000,
+    origin: 'top',
+    reset: false,
+  });
 });
