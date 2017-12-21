3abe51815b506214544d034e9007327323de2048
Simon Guillorit <simon.guillorit@gmail.com>
2017-12-21 19:15:23 +0100

    Add Facebook Oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#landing'
 
