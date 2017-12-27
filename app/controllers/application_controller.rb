22599526501c1f4d5c762dd22c8210d1ad139e75
Simon Guillorit <simon.guillorit@gmail.com>
2017-12-27 11:50:46 +0100

    Add first_name field for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
 
   def after_sign_in_path_for(user)
@@ -19,6 +21,10 @@ class ApplicationController < ActionController::Base
   # end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
