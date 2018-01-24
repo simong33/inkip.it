c01b4790d7542ad92205c385f49931833cfc7f44
Simon Guillorit <simon.guillorit@gmail.com>
2018-01-24 18:17:44 +0100

    Fix sidbar items css on active

--- app/assets/javascripts/profile_page.js
+++ app/assets/javascripts/profile_page.js
@@ -22,28 +22,40 @@ $(document).ready(function(){
 
 // ADDRESS AUTOCOMPLETE
 
-function initializeAutocomplete(id) {
-  var element = document.getElementById(id);
-  if (element) {
-    var autocomplete = new google.maps.places.Autocomplete(element, { types: ['(cities)'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-  }
+// function initializeAutocomplete(id) {
+//   var element = document.getElementById(id);
+//   if (element) {
+//     var autocomplete = new google.maps.places.Autocomplete(element, { types: ['(cities)'] });
+//     google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+//   }
+// }
+
+// function onPlaceChanged() {
+//   var place = this.getPlace();
+
+//   for (var i in place.address_components) {
+//     var component = place.address_components[i];
+//     for (var j in component.types) {  // Some types are ["country", "political"]
+//       var type_element = document.getElementById(component.types[j]);
+//       if (type_element) {
+//         type_element.value = component.long_name;
+//       }
+//     }
+//   }
+// }
+
+// google.maps.event.addDomListener(window, 'load', function() {
+//   initializeAutocomplete('user-location-input');
+// });
+
+function initialize() {
+
+ var options = {
+  types: ['(cities)']
+ };
+
+ var input = document.getElementById('user-location-input');
+ var autocomplete = new google.maps.places.Autocomplete(input, options);
 }
 
-function onPlaceChanged() {
-  var place = this.getPlace();
-
-  for (var i in place.address_components) {
-    var component = place.address_components[i];
-    for (var j in component.types) {  // Some types are ["country", "political"]
-      var type_element = document.getElementById(component.types[j]);
-      if (type_element) {
-        type_element.value = component.long_name;
-      }
-    }
-  }
-}
-
-google.maps.event.addDomListener(window, 'load', function() {
-  initializeAutocomplete('user-location-input');
-});
+google.maps.event.addDomListener(window, 'load', initialize);
