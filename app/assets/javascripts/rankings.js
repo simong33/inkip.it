bb01faee6c5269d3c7ea14551c77daed934952bc
Simon Guillorit <simon.guillorit@gmail.com>
2018-01-26 17:13:49 +0100

    Remove comments in ranking.js

--- app/assets/javascripts/rankings.js
+++ app/assets/javascripts/rankings.js
@@ -1,45 +1,4 @@
 $(document).ready(function(){
 
-  // $(".sidebar-item--authors_mean").click(function(event){
-  //   event.preventDefault();
-
-  //   if (!$(this).hasClass('sidebar-item--active')) {
-  //     $('.sidebar-item').removeClass('sidebar-item--active');
-  //     $(this).addClass("sidebar-item--active");
-
-  //     $(".content-dashboard").each(function(){
-  //       $(this).addClass('hidden');
-  //       $('#ranking-mean').removeClass('hidden');
-  //     });
-  //   }
-  // });
-
-  // $(".sidebar-item--authors_max").click(function(event){
-  //   event.preventDefault();
-
-  //   if (!$(this).hasClass('sidebar-item--active')) {
-  //     $('.sidebar-item').removeClass('sidebar-item--active');
-  //     $(this).addClass("sidebar-item--active");
-
-  //     $(".content-dashboard").each(function(){
-  //       $(this).addClass('hidden');
-  //       $('#ranking-max').removeClass('hidden');
-  //     });
-  //   }
-  // });
-
-  // $(".sidebar-item--consistent_authors").click(function(event){
-  //   event.preventDefault();
-
-  //   if (!$(this).hasClass('sidebar-item--active')) {
-  //     $('.sidebar-item').removeClass('sidebar-item--active');
-  //     $(this).addClass("sidebar-item--active");
-
-  //     $(".content-dashboard").each(function(){
-  //       $(this).addClass('hidden');
-  //       $('#ranking-streaks').removeClass('hidden');
-  //     });
-  //   }
-  // });
 
 });
