b7ebe2150803cebcf37422123a260ee110bf35ad
Simon Guillorit <simon.guillorit@gmail.com>
2018-01-26 17:13:15 +0100

    Ajaxified rankings page'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,39 @@ class PagesController < ApplicationController
   end
 
   def rankings
-    @best_authors_max = User.best_authors_max
-    @best_authors_mean = User.best_authors_mean
-    @most_consistent_authors = User.most_consistent_authors
+
+    case params["filter"]
+
+    when "words_max"
+      @authors = User.best_authors_max
+      @filter = "words_max"
+
+      respond_to do |format|
+          format.js
+      end
+
+    when "words_mean"
+      @authors = User.best_authors_mean
+      @filter = "words_mean"
+
+      respond_to do |format|
+          format.js
+      end
+
+    when "streaks_max"
+      @authors = User.most_consistent_authors
+      @filter = "streaks_max"
+      @streaks = "streaks_max"
+
+      respond_to do |format|
+          format.js
+      end
+
+    else
+
+      @authors = User.best_authors_max
+
+    end
   end
 
   def privacy_policy
